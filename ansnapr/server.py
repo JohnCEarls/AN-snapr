9fe377562b4adf0b94e9ae0ba2069c6220a47a23
John C. Earls <john.c.earls@gmail.com>
2014-07-18 01:54:55 +0000

    Getting server started

--- ansnapr/server.py
+++ ansnapr/server.py
@@ -1,3 +1,27 @@
+
 def main():
+    from ansnapr.utils import debug
+    import masterdirac.models.systemdefaults as sys_def
+    debug.InitLogging()
+    local = sys_def.get_system_defaults( component='Master', 
+            setting_name="local_settings")
+    init_q = local['init-queue']
+    directories = {}
+    directories = sys_def.get_system_defaults(component='SNAPR',
+            setting_name='directories')
+
+    try:
+        running = True
+        while running:
+            s = SNAPr( directories, init_q )
+            d.run()
+            running = d.restart
+            if not running:
+                d.terminate_response()
+    except:
+        logger = logging.getLogger("server.main")
+        logger.exception("Process killing error")
+
+if __name__ == "__main__":
     #the server initialization will go here
-    print "TODO"
+    print "Executions starts in main()"
