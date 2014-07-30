7d4d07e552f4d144ee20bb3bee1c22061fe2b74d
John C. Earls <john.c.earls@gmail.com>
2014-07-30 00:17:32 +0000

    Minor changes, mainly got to the point I realized I need synapse integration.

--- ansnapr/sub.py
+++ ansnapr/sub.py
@@ -1,5 +1,5 @@
 import logging
-import multiprocessing
+import subprocess
 
 def build_genome_index( genome_file, index_dir, **kwargs ):
     """
@@ -26,7 +26,11 @@ def build_genome_index( genome_file, index_dir, **kwargs ):
                    about 30%, depending on the other index parameters and the contents of the reference genome
 
     """
-    print _gi_command( genome_file, index_dir, **kwargs)
+    command =  _gi_command( genome_file, index_dir, **kwargs)
+    sn_sp = subprocess.Popen( command, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE, shell=True )
+    #TODO: log_subprocess_messages( 
+    
 
 def _gi_command( genome_file, index_dir, **kwargs ):
     command = "snapr index %s %s " % (genome_file, index_dir)
@@ -67,13 +71,18 @@ def build_transcriptome_index( annotation_file, genome_file, transcriptome_dir, 
 
 
 def _ti_command( annotation_file, genome_file, transcriptome_dir, **kwargs ):
-    command = "snapr transcriptome %s %s %s" %  ( annotation_file, genome_file, transcriptome_dir)
+    command = "snapr transcriptome %s %s %s" %  ( 
+            annotation_file, genome_file, transcriptome_dir)
     for key, value in kwargs.iteritems():
         if value:
             command = "%s -%s%s" % (command, key, value)
         else:
             command = "%s -%s" % (command, key )
     return command
+
+def align_transcriptome( read_type, index_dir, transcript_dir, annotation, read_file, out_file , sort=False):
+    command = _align_command(  read_type, index_dir, transcript_dir, 
+            annotation, read_file, out_file , sort)
 
 def _align_command( read_type, index_dir, transcript_dir, annotation, read_file, out_file , sort=False):
     command = "snapr %s %s %s %s %s -o %s" % ( read_type, 
@@ -85,6 +94,49 @@ def _align_command( read_type, index_dir, transcript_dir, annotation, read_file,
     if sort:
         command += " -so"
     return command
+
+
+def log_subprocess_messages( sc_p, q, base_message):
+    """
+    Reads messages from stdout/stderr and writes them to
+    given queue(q).
+
+    Used on cluster startup to observe progress
+
+    sc_p : subprocess that is launching the cluster
+    q : boto.sqs.Queue that accepts messages
+    base_message : dictionary that contains the message template
+    """
+    def send_msg( mtype, msg, base_message=base_message, q=q, acc = {'i':0}):
+        log_message = base_message.copy()
+        log_message['time'] = datetime.utcnow().isoformat()
+        log_message['type'] = mtype
+        log_message['msg'] = msg
+        log_message['count'] = acc['i']
+        q.write( Message( body=json.dumps( log_message ) ) )
+        acc['i'] =acc['i'] + 1
+
+    send_msg('system', 'Starting')
+    cont = True
+    reads = (sc_p.stdout, sc_p.stderr)
+    while cont:
+        cont = sc_p.poll() is None
+        ret = select.select(reads, [], [])
+        for fd in ret[0]:
+            if fd.fileno() == sc_p.stdout.fileno():
+                send_msg('stdout', sc_p.stdout.readline().strip() )
+            if fd.fileno() == sc_p.stderr.fileno():
+                send_msg('stderr', sc_p.stderr.readline().strip() )
+    line = sc_p.stdout.readline().strip()
+    while line != '':
+        send_msg('stdout',line)
+        line = sc_p.stdout.readline().strip()
+    line = sc_p.stderr.readline().strip()
+    while line != '':
+        send_msg('stderr', line)
+        line = sc_p.stderr.readline().strip()
+    send_msg( 'system', 'Complete: returned[%i]' % cont )
+    return cont
 
 if __name__ == "__main__":
     print "Subprocesses test"
